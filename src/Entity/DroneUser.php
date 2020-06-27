278ba6dfd44f94c757dd62c901f655f79642fd3d
Thierry Kg <t.kaing13@gmail.com>
2020-06-27 15:53:24 +0200

    - make unique entity working

--- src/Entity/DroneUser.php
+++ src/Entity/DroneUser.php
@@ -11,8 +11,12 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\DroneUserRepository")
  * @UniqueEntity(
- *     fields={"email", "pseudo"},
- *     message="It's already in use on app."
+ *     fields="email",
+ *     message="Email already in use on app."
+ * )
+ * @UniqueEntity(
+ *     fields="pseudo",
+ *     message="Pseudo already in use on app."
  * )
  */
 class DroneUser
